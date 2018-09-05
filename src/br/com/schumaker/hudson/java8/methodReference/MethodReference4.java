905a2490570a1552b8f6d8c35f1046357318746d
Hudson Schumaker <hudson@schumakerteam.com>
2018-09-05 20:52:06 -0300

    Method reference ex4.

---
+++ src/br/com/schumaker/hudson/java8/methodReference/MethodReference4.java
@@ -0,0 +1,43 @@
+package br.com.schumaker.hudson.java8.methodReference;
+
+import br.com.schumaker.hudson.java8.lambdas.Usuario;
+import java.util.ArrayList;
+import static java.util.Comparator.comparingInt;
+import java.util.List;
+
+/**
+ *
+ * @author hudsonschumaker
+ */
+public class MethodReference4 {
+    public static void main(String... args) {
+        Usuario user1 = new Usuario("Henrique Schumaker", 50);
+        Usuario user2 = new Usuario("Humberto Schumaker", 120);
+        Usuario user3 = new Usuario("Hugo Schumaker", 190);
+        Usuario user4 = new Usuario("Hudson Schumaker", 10);
+        Usuario user5 = new Usuario("Gabriel Schumaker", 90);
+        Usuario user6 = new Usuario("Nikolas Schumaker", 290);
+        Usuario user7 = new Usuario("Elisabeth Schumaker", 195);
+        Usuario user8 = new Usuario("Eliza Schumaker", 1000);
+        Usuario user9 = new Usuario("Marcos Schumaker", 100);
+        Usuario user10 = new Usuario("Wilson Schumaker", 1300);
+        
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(user1);
+        usuarios.add(user2);
+        usuarios.add(user3);
+        usuarios.add(user4);
+        usuarios.add(user5);
+        usuarios.add(user6);
+        usuarios.add(user7);
+        usuarios.add(user8);
+        usuarios.add(user9);
+        usuarios.add(user10);
+        
+        //Comparar com methods reference usando comparingInt para evitar autoBoxing
+        usuarios.sort(comparingInt(Usuario::getPontos));
+        
+        //method reference chamando metodo q recebe parametro
+        usuarios.forEach(System.out::println);
+     }
+}
