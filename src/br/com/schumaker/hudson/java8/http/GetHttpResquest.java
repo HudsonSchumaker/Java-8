70dfa6404809e1160ecdbd68be1c31edd09015bd
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2020-09-15 19:47:16 +0200

    Update GetHttpResquest.java

--- src/br/com/schumaker/hudson/java8/http/GetHttpResquest.java
+++ src/br/com/schumaker/hudson/java8/http/GetHttpResquest.java
@@ -1,5 +1,12 @@
 package br.com.schumaker.hudson.java8.http;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  *
  * @author hudson schumaker
