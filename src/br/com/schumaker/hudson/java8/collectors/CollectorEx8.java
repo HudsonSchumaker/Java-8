f1a86f166745a6c82b58b4f2bf9301f73223bbae
HudsonSchumaker <hudson.schumaker@me.com>
2023-10-05 16:24:20 +0200

    collector to map example

---
+++ src/br/com/schumaker/hudson/java8/collectors/CollectorEx8.java
@@ -0,0 +1,26 @@
+package br.com.schumaker.hudson.java8.collectors;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ *
+ * @author Hudson Schumaker
+ */
+public class CollectorEx8 {
+
+    public static void main(String... args) {
+        List<String> fruits = Arrays.asList("apple", "banana", "cherry", "date","blackberry", "avocado");
+
+        Map<String, Integer> mapped = fruits.stream().collect(Collectors.toMap(
+                        f -> f,
+                        String::length
+        ));
+
+        mapped.forEach((k, v) -> System.out.println(k + ": " + v));
+
+    }
+}
